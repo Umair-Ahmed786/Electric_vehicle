84a1c168c19e72af8f3603c451fe6d51f7ee2836
Clément Nicolas <clement92nicolas@gmail.com>
2023-07-13 15:02:09 +0200

    Init + Configurator component + vehicles.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Configurator from "./components/Configurator/Configurator";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"><h1>Choix d'une voiture électrique</h1></header>
+            <section className="App-content">
+                <Configurator />
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Configurator/Configurator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Configurator.scss';
+
+const Configurator = () => (
+  <div className="configurator">
+    Configurator Component
+  </div>
+);
+
+export default Configurator;
