477e645efb6d011b0d7e49d0cb0a275f89d66b43
Clément Nicolas <clement92nicolas@gmail.com>
2023-07-13 16:39:31 +0200

    WIP talk between configurator and app + configurator inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Configurator from "./components/Configurator/Configurator";
+import vehicles from './vehicles.json'
 
 function App() {
+    let coefficients={};
+
     return (
         <div className="App">
             <header className="App-header"><h1>Choix d'une voiture électrique</h1></header>
             <section className="App-content">
-                <Configurator />
+                <Configurator coefficients={coefficients}/>
             </section>
+
+            <p>{coefficients}</p>
+            <code>{JSON.stringify(vehicles)}</code>
         </div>
     );
 }
--- src/components/Configurator/Configurator.js
+++ src/components/Configurator/Configurator.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Configurator.scss';
+import {Button, TextField} from "@mui/material";
 
 const Configurator = () => (
-  <div className="configurator">
-    Configurator Component
-  </div>
+    <div className="configurator">
+        <section className="Goods">
+            <h4>Avantages</h4>
+            <TextField id="coeff-trunk" label="Coefficient Coffre" value="2.5" type="number" size="small" step="0.5"
+                       margin="dense"/>
+            <TextField id="coeff-range" label="Coefficient Autonomie" value="4" type="number" size="small" step="0.5"
+                       margin="dense"/>
+            <TextField id="coeff-consumption" label="Coefficient Consommation" value="1" type="number" size="small"
+                       step="0.5" margin="dense"/>
+            <TextField id="coeff-supercharge" label="Coefficient Durée supercharge" value="2" type="number" size="small"
+                       step="0.5" margin="dense"/>
+            <TextField id="coeff-quality" label="Coefficient Quality" value="3.5" type="number" size="small"
+                       step="0.5" margin="dense"/>
+            <TextField id="coeff-practicality" label="Coefficient Praticité" value="1.5" type="number" size="small"
+                       step="0.5" margin="dense"/>
+            <TextField id="coeff-look" label="Coefficient Apparence" value="1" type="number" size="small" step="0.5"
+                       margin="dense"/>
+        </section>
+        <section className="Bads">
+            <h4>Inconvénients</h4>
+            <TextField id="coeff-price" label="Coefficient Prix" value="1.5" type="number" size="small" step="0.5"
+                       margin="dense"/>
+            <TextField id="coeff-volume" label="Coefficient Volume" value="2.5" type="number" size="small" step="0.5"
+                       margin="dense"/>
+        </section>
+        <Button onClick={() => ({a: 1})}>Calculer</Button>
+    </div>
 );
 
 export default Configurator;
