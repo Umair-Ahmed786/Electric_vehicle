9234b0cca13b319a5389120197abf1b5cea9cbb9
Clément Nicolas <clement92nicolas@gmail.com>
2023-08-09 09:09:16 +0200

    feature(configurators): automatically trigger configurationChanged

--- src/components/ElectricConfigurator/ElectricConfigurator.js
+++ src/components/ElectricConfigurator/ElectricConfigurator.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import '../configurators.scss';
-import {Button, TextField} from "@mui/material";
+import {TextField} from "@mui/material";
 
 function ElectricConfigurator({configurationChanged}) {
     const [trunk, setTrunk] = useState(2.5);
@@ -13,70 +13,75 @@ function ElectricConfigurator({configurationChanged}) {
     const [volume, setVolume] = useState(1);
     const [practicality, setPracticality] = useState(4);
 
-    useEffect(() => {
-            configurationChanged({
-                trunk,
-                range,
-                consumption,
-                supercharge,
-                quality,
-                practicality,
-                look,
-                price,
-                volume
-            })
-        }, // eslint-disable-next-line react-hooks/exhaustive-deps
-        [])
+    const sendConfiguration = () => configurationChanged({
+        trunk, range, consumption, supercharge, quality, practicality, look, price, volume
+    })
 
-    return (
-        <div className="configurator">
-            <section className="goods">
-                <h4>Avantages</h4>
-                <TextField label="Coefficient Coffre" value={trunk} onChange={event => setTrunk(+event.target.value)}
-                           type="number" size="small" inputProps={{step: 0.5}}
-                           margin="dense"/>
-                <TextField label="Coefficient Autonomie" value={range} onChange={event => setRange(+event.target.value)}
-                           type="number" size="small" inputProps={{step: 0.5}}
-                           margin="dense"/>
-                <TextField label="Coefficient Consommation" value={consumption}
-                           onChange={event => setConsumption(+event.target.value)} type="number" size="small"
-                           inputProps={{step: 0.5}} margin="dense"/>
-                <TextField label="Coefficient Durée supercharge" value={supercharge}
-                           onChange={event => setSupercharge(+event.target.value)} type="number" size="small"
-                           inputProps={{step: 0.5}} margin="dense"/>
-                <TextField label="Coefficient Quality" value={quality}
-                           onChange={event => setQuality(+event.target.value)} type="number" size="small"
-                           inputProps={{step: 0.5}} margin="dense"/>
-                <TextField label="Coefficient Apparence" value={look} onChange={event => setLook(+event.target.value)}
-                           type="number" size="small" inputProps={{step: 0.5}}
-                           margin="dense"/>
-                <TextField label="Coefficient Praticité" value={practicality}
-                           onChange={event => setPracticality(+event.target.value)} type="number" size="small"
-                           inputProps={{step: 0.5}} margin="dense"/>
-            </section>
-            <section className="bads">
-                <h4>Inconvénients</h4>
-                <TextField label="Coefficient Prix" value={price} onChange={event => setPrice(+event.target.value)}
-                           type="number" size="small" inputProps={{step: 0.5}}
-                           margin="dense"/>
-                <TextField label="Coefficient Volume" value={volume} onChange={event => setVolume(+event.target.value)}
-                           type="number" size="small" inputProps={{step: 0.5}}
-                           margin="dense"/>
-            </section>
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(sendConfiguration, [])
 
-            <Button onClick={() => configurationChanged({
-                trunk,
-                range,
-                consumption,
-                supercharge,
-                quality,
-                practicality,
-                look,
-                price,
-                volume
-            })} variant="contained">Calculer</Button>
-        </div>
-    )
+    return (<div className="configurator">
+        <section className="goods">
+            <h4>Avantages</h4>
+            <TextField label="Coefficient Coffre" value={trunk} onChange={event => {
+                setTrunk(+event.target.value)
+                sendConfiguration()
+            }}
+                       type="number" size="small" inputProps={{step: 0.5}}
+                       margin="dense"/>
+            <TextField label="Coefficient Autonomie" value={range} onChange={event => {
+                setRange(+event.target.value)
+                sendConfiguration()
+            }}
+                       type="number" size="small" inputProps={{step: 0.5}}
+                       margin="dense"/>
+            <TextField label="Coefficient Consommation" value={consumption}
+                       onChange={event => {
+                           setConsumption(+event.target.value)
+                           sendConfiguration()
+                       }} type="number" size="small"
+                       inputProps={{step: 0.5}} margin="dense"/>
+            <TextField label="Coefficient Durée supercharge" value={supercharge}
+                       onChange={event => {
+                           setSupercharge(+event.target.value)
+                           sendConfiguration()
+                       }} type="number" size="small"
+                       inputProps={{step: 0.5}} margin="dense"/>
+            <TextField label="Coefficient Quality" value={quality}
+                       onChange={event => {
+                           setQuality(+event.target.value)
+                           sendConfiguration()
+                       }} type="number" size="small"
+                       inputProps={{step: 0.5}} margin="dense"/>
+            <TextField label="Coefficient Apparence" value={look} onChange={event => {
+                setLook(+event.target.value)
+                sendConfiguration()
+            }}
+                       type="number" size="small" inputProps={{step: 0.5}}
+                       margin="dense"/>
+            <TextField label="Coefficient Praticité" value={practicality}
+                       onChange={event => {
+                           setPracticality(+event.target.value);
+                           sendConfiguration()
+                       }} type="number" size="small"
+                       inputProps={{step: 0.5}} margin="dense"/>
+        </section>
+        <section className="bads">
+            <h4>Inconvénients</h4>
+            <TextField label="Coefficient Prix" value={price} onChange={event => {
+                setPrice(+event.target.value);
+                sendConfiguration()
+            }}
+                       type="number" size="small" inputProps={{step: 0.5}}
+                       margin="dense"/>
+            <TextField label="Coefficient Volume" value={volume} onChange={event => {
+                setVolume(+event.target.value);
+                sendConfiguration()
+            }}
+                       type="number" size="small" inputProps={{step: 0.5}}
+                       margin="dense"/>
+        </section>
+    </div>)
 }
 
 export default ElectricConfigurator;
--- src/components/HybridConfigurator/HybridConfigurator.js
+++ src/components/HybridConfigurator/HybridConfigurator.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import '../configurators.scss';
-import {Button, TextField} from "@mui/material";
+import {TextField} from "@mui/material";
 
 function HybridConfigurator({configurationChanged}) {
     const [trunk, setTrunk] = useState(2.5);
@@ -13,68 +13,83 @@ function HybridConfigurator({configurationChanged}) {
     const [volume, setVolume] = useState(1);
     const [practicality, setPracticality] = useState(4);
 
-    useEffect(() => {
-            configurationChanged({
-                trunk,
-                tank,
-                consumption,
-                supercharge,
-                quality,
-                practicality,
-                look,
-                price,
-                volume
-            })
-        },  // eslint-disable-next-line react-hooks/exhaustive-deps
-        [])
+    const sendConfiguration = () => configurationChanged({
+        trunk,
+        tank,
+        consumption,
+        supercharge,
+        quality,
+        practicality,
+        look,
+        price,
+        volume
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(sendConfiguration, [])
 
     return (
         <div className="configurator">
             <section className="goods">
                 <h4>Avantages</h4>
-                <TextField label="Coefficient Coffre" value={trunk} onChange={event => setTrunk(+event.target.value)}
+                <TextField label="Coefficient Coffre" value={trunk} onChange={event => {
+                    setTrunk(+event.target.value);
+                    sendConfiguration()
+                }}
                            type="number" size="small" inputProps={{step: 0.5}}
                            margin="dense"/>
-                <TextField label="Coefficient Réservoir" value={tank} onChange={event => setTank(+event.target.value)}
+                <TextField label="Coefficient Réservoir" value={tank} onChange={event => {
+                    setTank(+event.target.value);
+                    sendConfiguration()
+                }}
                            type="number" size="small" inputProps={{step: 0.5}}
                            margin="dense"/>
                 <TextField label="Coefficient Consommation" value={consumption}
-                           onChange={event => setConsumption(+event.target.value)} type="number" size="small"
+                           onChange={event => {
+                               setConsumption(+event.target.value);
+                               sendConfiguration()
+                           }} type="number" size="small"
                            inputProps={{step: 0.5}} margin="dense"/>
                 <TextField label="Coefficient Durée 'super'charge" value={supercharge}
-                           onChange={event => setSupercharge(+event.target.value)} type="number" size="small"
+                           onChange={event => {
+                               setSupercharge(+event.target.value);
+                               sendConfiguration()
+                           }} type="number" size="small"
                            inputProps={{step: 0.5}} margin="dense"/>
                 <TextField label="Coefficient Quality" value={quality}
-                           onChange={event => setQuality(+event.target.value)} type="number" size="small"
+                           onChange={event => {
+                               setQuality(+event.target.value);
+                               sendConfiguration()
+                           }} type="number" size="small"
                            inputProps={{step: 0.5}} margin="dense"/>
-                <TextField label="Coefficient Apparence" value={look} onChange={event => setLook(+event.target.value)}
+                <TextField label="Coefficient Apparence" value={look} onChange={event => {
+                    setLook(+event.target.value);
+                    sendConfiguration()
+                }}
                            type="number" size="small" inputProps={{step: 0.5}}
                            margin="dense"/>
                 <TextField label="Coefficient Praticité" value={practicality}
-                           onChange={event => setPracticality(+event.target.value)} type="number" size="small"
+                           onChange={event => {
+                               setPracticality(+event.target.value);
+                               sendConfiguration()
+                           }} type="number" size="small"
                            inputProps={{step: 0.5}} margin="dense"/>
             </section>
             <section className="bads">
                 <h4>Inconvénients</h4>
-                <TextField label="Coefficient Prix" value={price} onChange={event => setPrice(+event.target.value)}
+                <TextField label="Coefficient Prix" value={price} onChange={event => {
+                    setPrice(+event.target.value);
+                    sendConfiguration()
+                }}
                            type="number" size="small" inputProps={{step: 0.5}}
                            margin="dense"/>
-                <TextField label="Coefficient Volume" value={volume} onChange={event => setVolume(+event.target.value)}
+                <TextField label="Coefficient Volume" value={volume} onChange={event => {
+                    setVolume(+event.target.value);
+                    sendConfiguration()
+                }}
                            type="number" size="small" inputProps={{step: 0.5}}
                            margin="dense"/>
             </section>
-
-            <Button onClick={() => configurationChanged({
-                trunk,
-                tank,
-                consumption,
-                supercharge,
-                quality,
-                practicality,
-                look,
-                price,
-                volume
-            })} variant="contained">Calculer</Button>
         </div>
     )
 }
